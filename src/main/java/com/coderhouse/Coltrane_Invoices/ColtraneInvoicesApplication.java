fbcde4314b618b64cbc936fb126e12e814e51060
Tomás Pérez Mera <tperezmera@gmail.com>
2025-05-03 18:19:29 +0200

    Initial Commit.-

---
+++ src/main/java/com/coderhouse/Coltrane_Invoices/ColtraneInvoicesApplication.java
@@ -0,0 +1,15 @@
+package com.coderhouse.Coltrane_Invoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColtraneInvoicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColtraneInvoicesApplication.class, args);
+		
+
+	}
+
+}
---
+++ src/test/java/com/coderhouse/Coltrane_Invoices/ColtraneInvoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhouse.Coltrane_Invoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColtraneInvoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
