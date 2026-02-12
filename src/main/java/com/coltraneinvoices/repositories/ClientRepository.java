f7a3a3466f6c317cbc89df2b78c0550de1f20c26
Tomás Pérez Mera <tperezmera@gmail.com>
2026-02-12 22:23:54 +0100

    Removed old, unused Repos.-

--- src/main/java/com/coltraneinvoices/repositories/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coltraneinvoices.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coltraneinvoices.entities.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
-
--- src/main/java/com/coltraneinvoices/repositories/SaleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.coltraneinvoices.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coltraneinvoices.entities.Sale;
-
-public interface SaleRepository extends JpaRepository<Sale, Integer> {
-}
